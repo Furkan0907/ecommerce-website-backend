1539a2a6d726221ec4b23e542dfbd30732cd2065
Furkan Demirci <furkandemirci888@gmail.com>
2025-07-30 22:06:51 +0300

    Proje başlangıcı

---
+++ src/main/java/com/furkan/starter/EcommerceBackendApplicationStarter.java
@@ -0,0 +1,13 @@
+package com.furkan.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBackendApplicationStarter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBackendApplicationStarter.class, args);
+	}
+
+}
