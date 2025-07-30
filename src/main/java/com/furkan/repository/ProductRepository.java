71352dbc110596935ae7177d2b655eda4f71dd62
Furkan Demirci <furkandemirci888@gmail.com>
2025-07-31 01:26:25 +0300

    Modellerin repositorylerini oluşturduk.

---
+++ src/main/java/com/furkan/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.furkan.repository;
+
+import com.furkan.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
---
+++ src/main/java/com/furkan/repository/RefundRequestRepository.java
@@ -0,0 +1,9 @@
+package com.furkan.repository;
+
+import com.furkan.model.RefundRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RefundRequestRepository extends JpaRepository<RefundRequest, Long> {
+}
